70dae7c035052c9db2ab3505796ea2123ae2924c
Sai Amrutha <bsaiamrutha2005@gmail.com>
2025-07-26 15:33:00 +0530

    Create 4.java

---
+++ Java Fundamentals/Flow Control Statements/4.java
@@ -0,0 +1,14 @@
+/*Initialize two character variables in a program and display the characters in alphabetical order.
+Solution:
+*/
+Code : 
+public class Main{
+  public Static void main(String[] args){
+    char a = 'a';
+    char s = 'e';
+    if(a<s)
+      System.out.println(a+","+s);
+    else
+      System.out.println(s+","+a);
+  }
+}
