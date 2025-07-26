1b72923c005272a9ff823b80a09e4479f6f56d0e
Sai Amrutha <bsaiamrutha2005@gmail.com>
2025-07-26 14:38:09 +0530

    Delete Java Fundamentals/Language Fundamentals/2/2.java

--- Java Fundamentals/Language Fundamentals/2/2.java
+++
@@ -1,9 +0,0 @@
-/* 
- Write a Program to accept a String as a command line argument and print a Welcome message as given below.
-*/
-Code :
-public class Example{
-  public Static void main(String[] args){
-    System.out.println("Welcome" +args[0]);
-  }
-}
