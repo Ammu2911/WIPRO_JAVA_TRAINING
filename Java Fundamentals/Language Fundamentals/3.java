b305c79bbecd9c0ebc4291c1318f7519016be511
Sai Amrutha <bsaiamrutha2005@gmail.com>
2025-07-26 14:45:34 +0530

    Create 3.java

---
+++ Java Fundamentals/Language Fundamentals/3.java
@@ -0,0 +1,11 @@
+ /*Write a Program to accept two integers as command line arguments and print the sum of the two numbers
+Example1)
+C:\>java Sample 10 20
+O/P Expected : The sum of 10 and 20 is 30
+  */
+Code :
+public class Example{
+  public Static void main(String[] args){
+    System.out.println("The Sum of "args[0] +"and"+ args[1] "is" +(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
+  }
+}
