e578bbfdca3789f3086154c1ba8ea49ef2637b62
Sai Amrutha <bsaiamrutha2005@gmail.com>
2025-07-26 14:52:22 +0530

    Create 1.java

---
+++ Java Fundamentals/Flow Control Statements/1.java
@@ -0,0 +1,14 @@
+ /*A) Write a program to check if a given integer number is Positive, Negative, or Zero.*/
+Code :
+public class Example{
+  public Static void main(String[] args){
+  int a;
+    if(a>0)
+      System.out.println(a+ " is positive.");
+      else if(a<0)
+      System.out.println(a+ "is Negative.");
+      else
+    System.out.println(a+ "is Zero.");
+    }
+}
+    
