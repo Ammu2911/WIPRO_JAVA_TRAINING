30b0f74a0d2b464a5b9b95594a3ecc85a2b70165
Sai Amrutha <bsaiamrutha2005@gmail.com>
2025-07-26 14:32:06 +0530

    Create 1.java

---
+++ Java Fundamentals/Language Fundamentals/1.java
@@ -0,0 +1,10 @@
+/*Write a Program that accepts two Strings as command line arguments and generate the output in the required format. 
+
+Example1)
+If the two command line arguments are Wipro and Bangalore then the output generated should be Wipro Technologies Bangalore.*/
+Code :
+public class Examplle{
+  public Static void main(String[] args){
+    System.out.println(args[0]+ "Technologies" +args[1]);
+  }
+}
