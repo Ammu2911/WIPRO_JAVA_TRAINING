fa1c797d7d70e5b852087a07424af02592b6a2d3
Sai Amrutha <bsaiamrutha2005@gmail.com>
2025-07-26 14:36:17 +0530

    Create 2.java

---
+++ Java Fundamentals/Language Fundamentals/2/2.java
@@ -0,0 +1,9 @@
+/* 
+ Write a Program to accept a String as a command line argument and print a Welcome message as given below.
+*/
+Code :
+public class Example{
+  public Static void main(String[] args){
+    System.out.println("Welcome" +args[0]);
+  }
+}
