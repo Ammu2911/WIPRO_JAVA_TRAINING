7db77a5fe70fa5cbd95eef3e1cd1e31e46ca3e97
Sai Amrutha <bsaiamrutha2005@gmail.com>
2025-07-27 22:18:04 +0530

    Create 15.java

---
+++ Java Fundamentals/Flow Control Statements/15.java
@@ -0,0 +1,25 @@
+/*Write a program to print * in Floyds format (using for and while loop)
+*
+*  *
+*  *   *
+
+Solution:
+*/
+public class Main {
+	public static void main(String[] args) {
+		if(args.length!=0)		
+		{	
+      int n = Integer.parseInt(args[0]);
+			for (int i=0 ;i<n;i++)
+ 	{
+				for (int j=0;j<=i ;j++ ) 
+{
+					System.out.print("*"+" ");
+				}
+				System.out.println();
+			}
+		}
+		else
+			System.out.println("Please enter an integer number");	
+	}
+}
